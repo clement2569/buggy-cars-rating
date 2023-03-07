b6d3a73a0494d7626bc88572439c4f729eaf5f9e
clement2569 <127062902+clement2569@users.noreply.github.com>
2023-03-07 13:05:40 +1300

    Buggy Car Rating code
    Committed automation test code

---
+++ Buggy_Cars_Rating/Buggy_Cars_Rating/Buggy_Cars_Rating.cs
@@ -0,0 +1,112 @@
+using Newtonsoft.Json;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+
+
+namespace Buggy_Cars_Rating
+{
+    public class Buggy_Cars_Rating : IDisposable
+    {
+        private IWebDriver _driver;
+        private readonly string _homeUrl, _alfaRomeoModelUrl, _registerUrl, _password;
+        private string _username;
+        private Utilities _utils;
+
+
+        public Buggy_Cars_Rating() 
+        {
+            _utils = new Utilities();
+            _homeUrl = "https://buggy.justtestit.org";
+            _alfaRomeoModelUrl = _homeUrl+"/model/c4u1mqnarscc72is00ng%7Cc4u1mqnarscc72is00sg";
+            _registerUrl = _homeUrl + "/register";
+            _driver = new ChromeDriver();
+            _username = "automated_user_";
+            _password = "Testing123!";
+            _driver.Manage().Window.Maximize();
+        }
+        public void Dispose()
+        {
+            _driver.Dispose();
+        }
+
+
+        //can do inline theory for all the models urls if considered necessary
+        [Fact]
+        public void TestNoVotingWhileNotLoggedIn()
+        {
+            _driver.Navigate().GoToUrl(_alfaRomeoModelUrl);
+            Thread.Sleep(3000);
+            var loginRequiredMessage = _driver.FindElement(By.XPath("//p[@class='card-text']")).Text;
+
+            //verify no vote button
+            Assert.Empty(_driver.FindElements(By.XPath("//button[@class='btn btn-success' and text()='Vote!']")));
+            //verify no comment box
+            Assert.Empty(_driver.FindElements(By.XPath("//textarea[@id='comment']")));
+            //verify login requirement message
+            Assert.Equal("You need to be logged in to vote.", loginRequiredMessage);
+        }
+        [Fact]
+        public void TestSuccessfulRegistration()
+        {
+            //register a new account
+            _driver.Navigate().GoToUrl(_registerUrl);
+            Thread.Sleep(3000);
+
+            //Generate a new login name
+            string loginName = Utilities.GenerateNewLogin(_username);
+
+            //Register using generated login name and predetermined valid password.
+            Utilities.RegisterNewUser(_driver, loginName,_password);
+
+            var registrationSuccess = _driver.FindElement(By.XPath("//div[@class='result alert alert-success']"));
+
+            //verify registration success popup message and check if displayed
+            Assert.Equal("Registration is successful", registrationSuccess.Text);
+            Assert.True(registrationSuccess.Displayed);
+
+            //login
+            Utilities.Login(_driver, loginName, _password);
+
+            var loginGreeting = _driver.FindElement(By.XPath("//span[@class='nav-link disabled']"));
+
+            //verify login greeting message and check if displayed
+            Assert.Equal("Hi, First", loginGreeting.Text);
+            Assert.True(loginGreeting.Displayed);
+        }
+
+        [Fact]
+        public void TestVoteWithoutComment()
+        
+        {
+            _driver.Navigate().GoToUrl(_registerUrl);
+            Thread.Sleep(3000);
+
+            //Generate a new login name
+            string loginName = Utilities.GenerateNewLogin(_username);
+
+            //Register using generated login name and predetermined valid password.
+            Utilities.RegisterNewUser(_driver, loginName, _password);
+
+            _driver.Navigate().GoToUrl(_alfaRomeoModelUrl);
+
+            //login
+            Utilities.Login(_driver, loginName, _password);
+            Thread.Sleep(3000);
+            //get the vote count before pressing vote
+            int beforeVoteCount = Int32.Parse(_driver.FindElement(By.XPath("//h4//strong")).Text);
+            Thread.Sleep(3000);
+
+            _driver.FindElement(By.XPath("//button[@class='btn btn-success' and text()='Vote!']")).Click();
+            Thread.Sleep(3000);
+            int afterVoteCount = Int32.Parse(_driver.FindElement(By.XPath("//h4//strong")).Text);
+            Thread.Sleep(3000);
+            var voteThank = _driver.FindElement(By.XPath("//div//p[@class='card-text']"));
+
+            //verify the vote count is incremented by 1 after voting.
+            Assert.Equal(afterVoteCount, beforeVoteCount + 1);
+            //verify thank you message on voting.
+            Assert.Equal("Thank you for your vote!", voteThank.Text);
+        }
+    }
+}
---
+++ Buggy_Cars_Rating/Buggy_Cars_Rating/Utilities.cs
@@ -0,0 +1,43 @@
+﻿using OpenQA.Selenium;
+
+namespace Buggy_Cars_Rating
+{
+    internal class Utilities
+    {
+
+        //Generates new login name by adding a number based on entries in text file.
+        public static string GenerateNewLogin(string _username)
+        {
+            string path = "./Automated_Users_List.txt";
+            var textLines = File.ReadAllLines(path);
+            string loginName = _username + (textLines.Count() + 1).ToString();
+            using (StreamWriter w = File.AppendText(path))
+            {
+                w.WriteLine(loginName);
+            }
+            return loginName;
+        }
+
+        public static void RegisterNewUser(IWebDriver _driver, string loginName, string _password)
+        {
+            _driver.FindElement(By.Id("username")).SendKeys(loginName);
+            _driver.FindElement(By.Id("firstName")).SendKeys("First");
+            _driver.FindElement(By.Id("lastName")).SendKeys("Last");
+
+            _driver.FindElement(By.Id("password")).SendKeys(_password);
+            _driver.FindElement(By.Id("confirmPassword")).SendKeys(_password);
+
+            Thread.Sleep(3000);
+            _driver.FindElement(By.XPath("//button[@type='submit' and text()='Register']")).Click();
+            Thread.Sleep(3000);
+        }
+
+        public static void Login(IWebDriver _driver,string loginName, string password)
+        {
+            _driver.FindElement(By.Name("login")).SendKeys(loginName);
+            _driver.FindElement(By.Name("password")).SendKeys(password);
+            _driver.FindElement(By.XPath("//button[@type='submit' and text()='Login']")).Click();
+            Thread.Sleep(2000);
+        }
+    }
+}
